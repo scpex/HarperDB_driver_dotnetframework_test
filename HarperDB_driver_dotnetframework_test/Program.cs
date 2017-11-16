790617e75a0949c34203d4d964e255f048b33f9a
scpex <mr.nathakit@live.com>
2017-11-16 23:19:23 +0700

    select json data from my harperdb server
    CData ADO driver test case

---
+++ HarperDB_driver_dotnetframework_test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using HarperDB.NET;
+
+namespace HarperDB_driver_dotnetframework_test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAsync().GetAwaiter().GetResult();
+            Console.ReadKey();
+        }
+        static async Task<dynamic> GetAsync()
+        {
+            HarperDBClient harperDB = new HarperDBClient(new Uri("http://52.187.61.90:9925"), "HDB_ADMIN", "root1984");
+            var response = await harperDB.SQLSelect(@"select productid as id,* from northwnd.products order by id asc limit 10");
+            var data = JsonConvert.SerializeObject(response);
+            foreach (var pair in response)
+            {
+                Console.Write(pair);    // json data
+            }
+            //Console.WriteLine(data);  // json object
+            return data;
+        }
+    }
+}
